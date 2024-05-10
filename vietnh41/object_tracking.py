13c0da1776207894bf72c14b5cb9c1f843571fb6
VietNguyenHoang812 <hoangviet1121999@gmail.com>
2024-05-10 21:55:50 +0700

    update: demo video

--- vietnh41/object_tracking.py
+++ vietnh41/object_tracking.py
@@ -3,15 +3,15 @@ import numpy as np
 import time
 
 from collections import defaultdict
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 from ultralytics import YOLO
-from ultralytics.utils.checks import check_imshow
 from ultralytics.utils.plotting import Annotator, colors
 
 
 track_history = defaultdict(lambda: [])
 model = YOLO("yolov8s.pt")
 names = model.model.names
-
 
 video_path = "vietnh41/instrusion_example.mp4"
 cap = cv2.VideoCapture(video_path)
@@ -27,23 +27,23 @@ prev_frame_time = 0
 new_frame_time = 0
 down = {}
 counter_down = set()
+insize_zone = set()
 
 top_left  = (1094, 319)
 top_right = (1330, 315)
 bot_left  = (1126, 538)
 bot_right = (1407, 530)
 zone = [top_left, top_right, bot_right, bot_left]
+polygon = Polygon(zone)
 
 while cap.isOpened():
     success, frame = cap.read()
     if success:
-        results = model.track(frame, persist=True, verbose=False, tracker='bytetrack.yaml')
+        results = model.track(frame, persist=True, verbose=False, tracker='bytetrack.yaml', classes = [0])
         boxes = results[0].boxes.xyxy.cpu()
         red_color = (0, 0, 255)
-        # y = 308
-        # offset = 7
 
-        # counter line
+        # Instrusion detection zone
         cv2.polylines(frame, np.array([zone]), True, (255, 0, 0), 2)
 
         if results[0].boxes.id is not None:
@@ -51,18 +51,14 @@ while cap.isOpened():
             clss = results[0].boxes.cls.cpu().tolist()
             track_ids = results[0].boxes.id.int().cpu().tolist()
             confs = results[0].boxes.conf.float().cpu().tolist()
-            annotated_frame = results[0].plot()
-            frame = annotated_frame
-            
+
             # Annotator Init
             annotator = Annotator(frame, line_width=2)
 
             for box, cls, track_id in zip(boxes, clss, track_ids):
+                x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
                 label_name = names[int(cls)]
-                if label_name != "person":
-                    continue
-                # annotator.box_label(box, color=colors(int(cls), True), label=label_name)
-
+                
                 # Store tracking history
                 track = track_history[track_id]
                 track.append((int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)))
@@ -70,27 +66,24 @@ while cap.isOpened():
                     track.pop(0)
 
                 # Plot tracks
-                points = np.array(track, dtype=np.int32).reshape((-1, 1, 2))
-                # cv2.circle(frame, (track[-1]), 7, colors(int(cls), True), -1)
-                # cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2)
-                center_x = int((box[0] + box[2])/2)
-                footer_y = int(box[3])
+                center_x = int((x1+x2)/2)
+                footer_y = y2
+                point = Point([center_x, footer_y])
+                
+                if polygon.contains(point):
+                    insize_zone.add(track_id)
+                    annotator.box_label(box, color=colors(int(cls), True), label=label_name)
+                    # cv2.circle(frame, (center_x,footer_y), 5, (0,255,0), -1)
 
-                # if y < (cy + offset) and y > (cy - offset):
-                #     if track_id not in counter_down:
-                #         cv2.circle(frame, (cx,cy), 4, (0,0,255), -1)
-                #         cv2.putText(frame, str(len(counter_down)), (cx,cy), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0,255,255), 2)
-                #         counter_down.add(track_id)
+        cv2.putText(frame,('count: ')+ str(len(insize_zone)),(100,40),cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA) 
 
-        # cv2.putText(frame,('count: ')+ str(len(counter_down)),(60,40),cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA) 
+        new_frame_time = time.time()
+        fps = int(1/(new_frame_time-prev_frame_time))
+        prev_frame_time = new_frame_time
 
-        # new_frame_time = time.time()
-        # fps = int(1/(new_frame_time-prev_frame_time))
-        # prev_frame_time = new_frame_time
-
-        # infor = f"fps: {str(fps)} / count: {str(len(counter_down))}"
-        # cv2.putText(frame, infor, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color , 1, cv2.LINE_AA) 
-        # cv2.imshow('Frame', frame)
+        infor = f"fps: {str(fps)}"
+        cv2.putText(frame, infor, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color , 1, cv2.LINE_AA) 
+        cv2.imshow('Frame', frame)
         out.write(frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -100,5 +93,5 @@ while cap.isOpened():
 
 # result.release()
 cap.release()
-out.release()
+# out.release()
 cv2.destroyAllWindows()
